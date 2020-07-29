2d7bdaad1afb2d5af09a058a346deedfeb598772
Melisha <mslafaber1@gmail.com>
2020-07-29 19:51:53 +0930

    Created a new field to add images to the movie app.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -23,5 +23,7 @@ namespace MvcMovie.Models
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
+
+        public string Image { get; set; }
     }
 }
